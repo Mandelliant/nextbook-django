bd9c2670bac5620e2eafd3697688c2252fcb3d1c
Anthony <mandelliant@gmail.com>
2018-04-03 18:06:46 -0400

    code cleanup

--- djangosite/nextbook/models.py
+++ djangosite/nextbook/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Book(modesl.Model):
+class Book(models.Model):
     book_number = models.IntegerField(default=1)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
