5d2f33c18cf186476a175d60ab701ea85148c336
Anthony <mandelliant@gmail.com>
2018-04-04 21:02:55 -0400

    nextbook view

--- djangosite/menu/views.py
+++ djangosite/menu/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Do you want a suggestion or to add a new book?")
+    return HttpResponse("What would you like to do? \n1. Add a new book \n2. Get a reading suggestion")
+
+    menu_choice = input('Choose 1 or 2: ')
+    selection = int(menu_choice)
