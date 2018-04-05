b1857e13b99b19745dcfbf7cdd0a286c7307ca2e
Anthony <mandelliant@gmail.com>
2018-04-04 21:14:19 -0400

    nextbook model update

--- djangosite/nextbook/models.py
+++ djangosite/nextbook/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    book_number = models.IntegerField(default=1)
+    book_number = models.IntegerField(default=1, unique=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
 
