8af2acdef034d416ad513f9e6395e4c5f05bac0e
Anthony <mandelliant@gmail.com>
2018-06-03 12:37:48 -0400

    formatting

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -1,4 +1,5 @@
-"""djangosite URL Configuration
+"""
+djangosite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
