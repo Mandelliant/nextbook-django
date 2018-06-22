61d3253b573e962d41b1a80cdce8aa72eeb557b9
Anthony <mandelliant@gmail.com>
2018-06-22 19:20:13 -0400

    menu model work

--- djangosite/menu/models.py
+++ djangosite/menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    number = models.IntegerField
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=500)
+
+    def random(self):
+        random_book = random.choice
+        return random_book
+
+
+    # Placeholder for new book model.
+    def new_book(self):
+        fields = [nbt, author]
+
+        with open(self.csvfile, 'a', newline='') as library:
+
+            writer = csv.writer(library, delimiter=',')
+
+            line_number = Book.book_line(self)
+
+            writer.writerow([line_number] + fields)
+
+            newbook = '{} by {}'.format(nbt, author)
+
+            return "Added {}".format(newbook)
