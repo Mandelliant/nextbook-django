e35a2efd5d637ed31eedd54500c220cccb88b4a1
Anthony <mandelliant@gmail.com>
2018-06-30 12:28:32 -0400

    add gitignore

--- djangosite/menu/models.py
+++ djangosite/menu/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    number = models.IntegerField
-    title = models.CharField(max_length=500)
-    author = models.CharField(max_length=500)
+    book_number = models.IntegerField(default=1, unique=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
     def random(self):
         random_book = random.choice
