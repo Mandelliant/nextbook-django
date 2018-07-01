2c2a8dfe3e874a32d3ee71461cf231a031619a79
Anthony <mandelliant@gmail.com>
2018-06-30 23:17:51 -0400

    main menu form tweaks

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -19,8 +19,10 @@ from django.urls import include, path
 from nextbook.views import suggestion
 
 urlpatterns = [
+    path('', include('menu.urls')),
     path('menu/', include('menu.urls')),
     path('nextbook/', include('nextbook.urls')),
     path('newbook/', include('newbook.urls')),
+    path(r'?actions=newbook', include('newbook.urls')),
     path('admin/', admin.site.urls),
 ]
--- djangosite/newbook/urls.py
+++ djangosite/newbook/urls.py
@@ -5,4 +5,5 @@ from newbook.views import library
 urlpatterns = [
     path(r'newbook/', views.library, name='library'),
     path('', views.library, name='library'),
+    path(r'?actions=newbook', views.library, name='library'),
 ]
