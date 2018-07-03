7b8f4183d47b90e0852fa3903484a404272c9514
Anthony <mandelliant@gmail.com>
2018-07-02 20:56:33 -0400

    typo

--- djangosite/nextbook/models.py
+++ djangosite/nextbook/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
 
-    objects = modesl.Manager()
+    objects = models.Manager()
     randoms = RandomManager()
 
     def __str__(self):
