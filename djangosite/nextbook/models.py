4b00577d95a1d065693a8f7b112bd091dc64f290
Anthony <mandelliant@gmail.com>
2018-07-02 20:53:29 -0400

    update nextbook Book model with random manager

--- djangosite/nextbook/models.py
+++ djangosite/nextbook/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class RandomManager(models.Manager):
+
+    def get_query_set(self):
+        return super(RandomManager, self).get_query_set().order_by('?')
+
+
+
 class Book(models.Model):
     book_number = models.IntegerField(default=1, unique=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+
+    objects = modesl.Manager()
+    randoms = RandomManager()
 
     def __str__(self):
         return self.title
