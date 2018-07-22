d79a6ca9002f4cafa8abf9099ef45e6f05e0dfc0
Anthony <mandelliant@gmail.com>
2018-07-22 09:12:38 -0400

    removed success message from forms.py, reworked form clearing in views.py

--- djangosite/newbook/forms.py
+++ djangosite/newbook/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.db import models
 from django.forms import ModelForm
 from nextbook.models import Book
-from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import CreateView
 
 
 
@@ -14,14 +12,3 @@ class AddBook(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['book_number', 'title', 'author']
-
-
-class ComplicatedCreate(SuccessMessageMixin, CreateView):
-    model = Book
-    success_message = "%(calculated_field)s was created successfully"
-
-    def get_success_message(self, cleaned_data):
-        return self.success_message % dict(
-            cleaned_data,
-            calculated_field=self.object.calculated_field,
-        )
--- djangosite/newbook/views.py
+++ djangosite/newbook/views.py
@@ -3,12 +3,23 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from menu.models import Book
 from .forms import AddBook
+from nextbook.models import Book
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import CreateView
 
 
 def library(request):
-
+    form = AddBook(request.POST or None)
+    success = False
     if request.method == 'POST':
-
+        if form.is_valid():
+            addedbook = form.save(commit=False)
+            addedbook.save()
+            form = AddBook()
+            success = True
+    return render(request, 'newbook/index.html', {'form': form})
+'''
+    if request.method == 'POST':
         form = AddBook(request.POST)
 
         if form.is_valid():
@@ -20,3 +31,16 @@ def library(request):
         form = AddBook()
 
     return render(request, 'newbook/index.html', {'form': form})
+'''
+
+#all new below
+class BookAddConf(SuccessMessageMixin, CreateView):
+    model = Book
+    success_message = "%(new_title)s by %(new_author)s was added to your library"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            new_title=self.object.title,
+            new_author=self.object.author
+        )
