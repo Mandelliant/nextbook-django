5958074e391cc2691ff9e0f34fe617301c025a1f
Anthony <mandelliant@gmail.com>
2018-08-06 21:26:01 -0400

    updated view to clear form after submission

--- djangosite/newbook/views.py
+++ djangosite/newbook/views.py
@@ -5,25 +5,27 @@ from menu.models import Book
 from .forms import AddBook
 from nextbook.models import Book
 from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import CreateView
+
+
 
 
 
 def library(request):
     form = AddBook(request.POST or None)
+    complete_form = form
     success = False
     if request.method == 'POST':
         if form.is_valid():
             addedbook = form.save(commit=False)
             addedbook.save()
-            #form = AddBook()
+            form = AddBook()
             success = True
-            nbt = form.cleaned_data['title']
-            nba = form.cleaned_data['author']
+            nbt = complete_form.cleaned_data['title']
+            nba = complete_form.cleaned_data['author']
             messages.success(request, "{} by {} added to your library".format(nbt, nba))
 
-            #new_title = get_success_message()
+
+            #messages.success(request, "Book added")
 
 
     return render(request, 'newbook/index.html', {'form': form})
